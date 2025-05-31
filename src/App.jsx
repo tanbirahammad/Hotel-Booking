dd1556dfa4fb220ad6e79d56b09c167c5b009487
tanbirahammad <jht017933@gmail.com>
2025-06-01 02:45:09 +0600

    fixed deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Footer from "./components/Footer";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
 import MyBookings from "./pages/MyBookings";
+import HotelReg from "./components/HotelReg";
+import Layout from "./pages/hotelOwner/Layout";
+import Dashboard from "./pages/hotelOwner/Dashboard";
+import AddRoom from "./pages/hotelOwner/AddRoom";
+import ListRoom from "./pages/hotelOwner/ListRoom";
 
 function App() {
   
@@ -15,6 +20,7 @@ function App() {
     <>
     <div>
     {!isOwnerPath && <Navbar/>}
+  {false && <HotelReg/>}
 
 <div className="min-h-[70vh]">
 <Routes>
@@ -23,6 +29,12 @@ function App() {
   <Route path='/rooms' element={<AllRooms/>}/>
   <Route path='/rooms/:id' element={<RoomDetails/>}/>
   <Route path='/my-bookings' element={<MyBookings/>}/>
+  <Route path='/owner' element={<Layout/>}>
+    <Route index element={<Dashboard/>}/>
+    <Route path="add-room" element={<AddRoom/>}/>
+    <Route path="list-room" element={<ListRoom/>}/>
+
+  </Route>
 </Routes>
 </div>
 
